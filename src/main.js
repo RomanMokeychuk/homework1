76a5ecec8c2702e1f6e3fa9740038c01e2b83b12
RomanMokeychuk <mokeychuk@gmail.com>
2023-09-04 14:05:11 +0300

    homework four

--- src/main.js
+++ src/main.js
@@ -1,47 +1,76 @@
-// 1 програма
-const number = parseInt(prompt("Введіть число:")); //Поле для ввода числа
+//1 програма
 
-// Перевіряємо чи число ділиться на 3 та 5
-if (number % 3 === 0 && number % 5 === 0) {
-  console.log("FizzBuzz");
-  // Якщо число ділиться на 3
-} else if (number % 3 === 0) {
-  console.log("Fizz");
-  // Якщо число ділиться на 5
-} else if (number % 5 === 0) {
-  console.log("Buzz");
-} else {
-  // Якщо число не ділиться на 3 і 5
-  console.log(number); // Виводиця те саме число
-}
-
-// 2 програма
-const y = parseInt(prompt("Введіть рік:"));
-if ((y % 4 === 0 && y % 100 !== 0) || y % 400 === 0) {
-  console.log("YES"); // Високосний рік
-} else {
-  console.log("NO"); // Не високосний рік
-}
-
-// Програма 3
-// Отримуємо кількість років від користувача
-const year = parseInt(prompt("Введіть кількість років:"));
-function getYearLabel(number) {
-  if (number % 10 === 1 && number % 100 !== 11) {
-    return "рік"; // Якщо число закінчується на 1 (окрім 11), то використовуємо форму "рік"
-  } else if (
-    number % 10 >= 2 &&
-    number % 10 <= 4 &&
-    (number % 100 < 10 || number % 100 >= 20)
-  ) {
-    return "роки"; // Якщо закінчується на 2-4 (крім 12-14)
-  } else {
-    return "років"; // У всіх інших випадках "років"
+function isExemple(number) {
+  // Якщо число <= 1, то воно не є простим числом
+  if (number <= 1) return false;
+  // 2 та 3 - прості числа
+  if (number <= 3) return true;
+  // якщо число ділиться на 2 або 3, то воно складене
+  if (number % 2 === 0 || number % 3 === 0) return false;
+  let i = 5;
+  while (i * i <= number) {
+    // якщо число ділиться на i або (i + 2) то воно складене
+    if (number % i === 0 || number % (i + 2) === 0) return false;
+    i += 6;
   }
+  return true;
 }
-// Викликаємо функцію для визначення правильної форми слова "рік"
-const label = getYearLabel(year);
 
-console.log(`${year} ${label}`);
-//${year}: Це змінна year, яку ви виводим (кількість років)
-//${label}: Це змінна label, яку отримуєм в результаті виклику функції getYearLabel().
+const n = parseInt(prompt("Введіть число N:"));
+
+console.log(
+  isExemple(n)
+    ? `Число ${n} є простим числом.`
+    : `Число ${n} не є простим числом.`
+);
+
+//2 програма
+/*sumOfDivisors - це змінна, яка використовується для обчислення суми всіх дільників числа i в циклі,
+який перебирає можливі дільники цього числа.*/
+function findPerfectNumbers(N) {
+  const perfectNumbers = [];
+  // Перебираємо числа від 2 до N
+  for (let i = 2; i <= N; i++) {
+    // Починаємо з 1, бо кожне число ділиться на 1.
+    let sumOfDivisors = 1;
+
+    for (let k = 2; k * k <= i; k++) {
+      if (i % k === 0) {
+        sumOfDivisors += k;
+        // Якщо k не є квадратним коренем числа, то додаємо також його пару
+        if (k !== i / k) {
+          sumOfDivisors += i / k;
+        }
+      }
+    }
+    // Якщо сума дільників дорівнює самому числу, то воно досконале
+    if (sumOfDivisors === i) {
+      perfectNumbers.push(i);
+    }
+  }
+
+  return perfectNumbers;
+}
+
+const N = parseInt(prompt("Введіть число N:"));
+const perfectNumbers = findPerfectNumbers(N);
+
+console.log(
+  `Досконалі числа в діапазоні від 1 до ${N}: ${perfectNumbers.join(", ")}`
+);
+
+//3 програма
+
+var j = parseInt(prompt("Введіть висоту ялинки:"));
+var tree = "";
+
+for (var i = 1; i <= j; i++) {
+  // Генеруємо пробіли для вирівнювання
+  var spaces = "  ".repeat(j - i);
+  // Генеруємо зірочки
+  var stars = "* ".repeat(2 * i - 1);
+  // Об'єднуємо і додаємо до рядка
+  tree += spaces + stars + "\n";
+}
+
+alert(tree);
