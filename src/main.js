ad6fc007a1eff0958b35449bd50e89d5b206738a
RomanMokeychuk <mokeychuk@gmail.com>
2023-09-17 20:25:45 +0300

    homework eight

--- src/main.js
+++ src/main.js
@@ -1,91 +1,63 @@
-// //1 програма
-const inputArray = prompt(
-  "Введіть масив елементів, розділених комами (наприклад, 1,2,3,4,5):"
-);
-// Перетворення з введеного масиву в масив чисел
-const originalArray = inputArray.split(",").map(Number);
-//inputArray  розділяє рядок на підстроки, використовуючи кому (,) як роздільник
-//.map(Number) перетворює кожен елемент масиву з рядків у числа
-//reverse() розвертае масив
-originalArray.reverse();
-console.log(originalArray);
+//1 програма
+function capitalizeStrings(stringsArray) {
+  const capitalizedArray = [];
+  // Створюєм порожній масив для зберігання перетворених рядків
+  for (const str of stringsArray) {
+    // Перетворюєм поточний рядок, роблячи першу літеру великою (toUpperCase())
+    // і решту літер в нижньому регістрі (toLowerCase())
+    const capitalizedStr =
+      str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    // Додаєм перетворений рядок до масиву capitalizedArray
+    capitalizedArray.push(capitalizedStr);
+  }
+  // Повертаєм новий масив із перетвореними рядками
+  return capitalizedArray;
+}
+const inputString = prompt("Введите строки через запятую (без пробелов):");
+const words = inputString.split(",");
+console.log(capitalizeStrings(words));
 
 //2 програма
-function uniqueValues(array1, array2) {
-  //Множини Set з вхідних масивів для видалення дублікатів
-  const set1 = new Set(array1);
-  const set2 = new Set(array2);
-  // Об'єднуєм множини та створюємо нову множину, щоб зберегти унікальні значення
-  const uniqueSet = new Set([...set1, ...set2]);
-  // Перетворюєм множину у масив, щоб повернути результат у вигляді масиву.
-  return Array.from(uniqueSet);
+function findCommonElements(array1, array2) {
+  /*Використовуєм метод filter для створення нового масиву, в якому лишаються тільки елементи,
+    які є в обох вхідних масивах (array1 і array2)*/
+  const commonElements = array1.filter((element) => array2.includes(element));
+  return commonElements;
 }
-
-const inputArray1 = prompt(
-  "Введіть перший масив (використовуйте кому для розділення значень):"
+const input1 = prompt(
+  "Введіть числа через кому для першого масиву (без пробілів):"
 );
-const inputArray2 = prompt(
-  "Введіть другий масив (використовуйте кому для розділення значень):"
+const input2 = prompt(
+  "Введіть числа через кому для другого масиву (без пробілів):"
 );
-// Розділяєм введені рядки на масиви та перетворюємо їх елементи на числа.
-const array1 = inputArray1.split(",").map(Number);
-const array2 = inputArray2.split(",").map(Number);
-const uniqueValuesArray = uniqueValues(array1, array2);
-// Сортуєм масив з унікальними значеннями за зростанням.
-uniqueValuesArray.sort((a, b) => a - b);
 
-console.log(uniqueValuesArray);
+const array1 = input1.split(",").map(Number);
+const array2 = input2.split(",").map(Number);
+
+console.log(findCommonElements(array1, array2));
 
 //3 програма
-function calculateAverageGrade(students) {
-  const totalGrade = students.reduce((sum, student) => sum + student.grade, 0);
-  // Обчислюєм середній бал як відношення суми балів до кількості студентів
-  const averageGrade = totalGrade / students.length;
-  return averageGrade;
+function analyzeArray(numbersArray) {
+  // Використовуєм деструктуризацію для одразу отримати мінімальне та максимальне значення
+  const { min, max } = numbersArray.reduce(
+    (acc, num) => ({
+      min: Math.min(acc.min, num),
+      max: Math.max(acc.max, num),
+    }),
+    { min: Infinity, max: -Infinity }
+  );
+  // Обчислюєм суму елементів масиву за допомогою метода reduce
+  const sum = numbersArray.reduce((acc, num) => acc + num, 0);
+  // Обчислюєм середнє значення елементів, поділяючи суму на кількість елементів
+  const average = sum / numbersArray.length;
+  // Об'єкт з обчисленими значеннями
+  return {
+    sum,
+    average,
+    min,
+    max,
+  };
 }
-// Створюєм порожній масив для зберігання даних про студентів
-const students = [];
-
-function getStudentData() {
-  const name = prompt("Введіть ім'я студента:");
-  if (name === null) return null;
-  // Перевіряємо, чи користувач не скасував введення
-  const age = parseInt(prompt("Введіть вік студента:"));
-  const grade = parseFloat(prompt("Введіть середній бал студента:"));
-
-  return { name, age, grade };
-}
-
-function displayStudentList() {
-  console.log("Список студентів:");
-  students.forEach((student) => {
-    console.log(
-      `Ім'я: ${student.name}, Вік: ${student.age}, Бал: ${student.grade}`
-    );
-  });
-}
-
-while (true) {
-  const studentData = getStudentData();
-  if (studentData === null) break;
-  // Виходимо з циклу, якщо користувач скасував введення
-
-  if (
-    studentData.name &&
-    !isNaN(studentData.age) &&
-    !isNaN(studentData.grade)
-  ) {
-    // Додаєм дані студента до масиву students
-    students.push(studentData);
-  } else {
-    alert("Некоректні дані.");
-  }
-}
-
-displayStudentList();
-// Перевіряєм, чи є дані про студентів для обчислення середнього балу
-if (students.length > 0) {
-  console.log("Середній бал студентів:", calculateAverageGrade(students));
-} else {
-  console.log("Дані про студентів відсутні.");
-}
+const input = prompt("Введіть числа через кому (без пробілів):");
+const numbers = input.split(",").map(Number);
+console.log(analyzeArray(numbers));
