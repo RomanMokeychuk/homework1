902e9487317e4efabb0538a80fd4802aa28eb885
RomanMokeychuk <mokeychuk@gmail.com>
2023-10-02 10:50:36 +0300

    homework twelve

--- src/main.js
+++ src/main.js
@@ -1,47 +1,54 @@
 //1 програма
-function logArguments(fn) {
-    // Повертаєм нову функцію яка приймає будь-яку кількість аргументів за допомогою оператора rest (...)
-    return function (...args) {
-        console.log("Arguments:", args);
-        // Викликаєм оригінальну функцію fn, використовуючи метод apply передаючи їй аргументи
-        return fn.apply(this, args);
+function summarize(num) {
+    if (typeof num !== "number") {
+        num = 1;
+    }
+    // Функція яка додає число до аргументу
+    return function (arg) {
+        // Перевіряєм чи передано аргумент
+        if (typeof arg === "number") {
+            return num + arg;
+        } else {
+            // Додаємо одиницю якщо аргумент не передано
+            return num + 1;
+        }
     };
 }
-// Оголошуєм оригінальну функцію exampleFunction яка обчислює суму трьох чисел
-function exampleFunction(a, b, c) {
-    return a + b + c;
-}
-// Використовуєм функцію-декоратор logArguments для створення нової функції decoratedExampleFunction
-const decoratedExampleFunction = logArguments(exampleFunction);
-// Викликаєм decoratedExampleFunction з аргументами
-const result = decoratedExampleFunction(6, 8, 2);
-console.log("Result:", result);
+const addFive = summarize(10);
+console.log(addFive(2));
+console.log(addFive());
 
 //2 програма
-function validate(fn, validator) {
-    return function (...args) {
-        // Перевірка аргументів за допомогою валідатора
-        if (!validator(...args)) {
-            throw new Error("Invalid arguments");
-        }
-        return fn(...args);
+function counter(startValue, step) {
+    let current = startValue;
+    // Метод для збільшення значення на крок
+    function increment() {
+        current += step;
+    }
+    // Метод для зменшення значення на крок
+    function decrement() {
+        current -= step;
+    }
+    // Метод для скидання значення до початкового
+    function reset() {
+        current = startValue;
+    }
+    // Метод для отримання поточного значення
+    function getCurrentValue() {
+        return current;
+    }
+    // Повертаєм об'єкт з методами
+    return {
+        increment,
+        decrement,
+        reset,
+        getCurrentValue,
     };
 }
-// Валідатор який перевіряє чи всі аргументи є числами
-function numberValidator(...args) {
-    return args.every((arg) => typeof arg === "number");
-}
-function sumNumbers(a, b) {
-    return a + b;
-}
-// Створення верифікованої функції sumNumbers з використанням валідатора
-const validatedSumNumbers = validate(sumNumbers, numberValidator);
-// Виклик функції з логікою перевірки аргументів
-const outcome = validatedSumNumbers(6, 4);
-console.log("Result:", outcome);
-try {
-    // Спроба викликати функцію з неправильними аргументами
-    validatedSumNumbers(10, "invalid");
-} catch (error) {
-    console.error("Error:", error.message);
-}
+const myCounter = counter(10, 11);
+myCounter.increment();
+console.log(myCounter.getCurrentValue());
+myCounter.decrement();
+console.log(myCounter.getCurrentValue());
+myCounter.reset();
+console.log(myCounter.getCurrentValue());
