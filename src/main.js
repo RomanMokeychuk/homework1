3157a4056cb9732ffee5d38840493480c4f6dc20
RomanMokeychuk <mokeychuk@gmail.com>
2023-10-20 20:10:07 +0300

    homework sixteen

--- src/main.js
+++ src/main.js
@@ -1,29 +1,83 @@
-function findPalindrome(num) {
-    let steps = 0;
-    while (true) {
-        const numStr = String(num);
-        const reverseNumStr = numStr.split("").reverse().join("");
-        if (numStr === reverseNumStr) {
-            return { result: num, steps };
-        }
-        num += parseInt(reverseNumStr, 10);
-        steps++;
-        if (steps >= 1000) {
-            // Перевірка на Lychrel number
-            return { result: null, steps: -1 };
+//1 програма
+function randomDelayPrint(message) {
+    let index = 0;
+    function printNextLetter() {
+        if (index < message.length) {
+            const letter = message[index];
+            console.log(letter);
+            index++;
+
+            setTimeout(printNextLetter, Math.random() * 1000);
         }
     }
+    printNextLetter();
+}
+randomDelayPrint("Mokeychuk");
+
+//2 програма
+function debounce(func, delay) {
+    let timeoutId;
+    return function () {
+        clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(() => {
+            func.apply(this, arguments);
+        }, delay);
+    };
+}
+const expensiveOperation = () => console.log("Виконую складну операцію...");
+const debouncedExpensiveOperation = debounce(expensiveOperation, 1000);
+debouncedExpensiveOperation();
+debouncedExpensiveOperation();
+debouncedExpensiveOperation();
+
+//3 програма
+function intervalRace(functions, t) {
+    const results = [];
+    let index = 0;
+
+    const runNextFunction = () => {
+        if (index < functions.length) {
+            functions[index]().then((result) => {
+                results.push(result);
+                console.log(result);
+                index++;
+                if (index < functions.length) {
+                    setTimeout(runNextFunction, t);
+                }
+            });
+        }
+    };
+
+    runNextFunction();
 }
 
-// Використання функції
-const numberToCheck = 196;
-const result = findPalindrome(numberToCheck);
+// Приклад використання
+const task1 = () =>
+    new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Завдання 1 виконано");
+        }, 1000);
+    });
 
-if (result.steps === -1) {
-    console.log(
-        `Число ${numberToCheck} є Lychrel number - число яке немає поліндрому.`
-    );
-} else {
-    console.log(`Знайдено паліндром: ${result.result}`);
-    console.log(`Кількість кроків: ${result.steps}`);
-}
+const task2 = () =>
+    new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Завдання 2 виконано");
+        }, 500);
+    });
+
+const task3 = () =>
+    new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Завдання 3 виконано");
+        }, 1500);
+    });
+
+const task4 = () =>
+    new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Завдання 4 виконано");
+        }, 500);
+    });
+intervalRace([task1, task2, task3, task4], 1000);
