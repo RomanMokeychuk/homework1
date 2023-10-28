3d4f71ebf69f9ffe5817317f673b2921953b1526
RomanMokeychuk <mokeychuk@gmail.com>
2023-10-28 20:36:52 +0300

    homework seventeen

--- src/main.js
+++ src/main.js
@@ -1,83 +1,77 @@
 //1 програма
-function randomDelayPrint(message) {
-    let index = 0;
-    function printNextLetter() {
-        if (index < message.length) {
-            const letter = message[index];
-            console.log(letter);
-            index++;
-
-            setTimeout(printNextLetter, Math.random() * 1000);
-        }
-    }
-    printNextLetter();
+async function sumArrayPromise(array) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const sum = array.reduce((acc, current) => acc + current, 0);
+            resolve(sum);
+        }, 3000);
+    });
 }
-randomDelayPrint("Mokeychuk");
+// Використовуєм async/await для отримання результату з Promise
+(async () => {
+    const result = await sumArrayPromise([10, 20, 1, 50, 100]);
+    console.log(result);
+})();
 
 //2 програма
-function debounce(func, delay) {
-    let timeoutId;
-    return function () {
-        clearTimeout(timeoutId);
-
-        timeoutId = setTimeout(() => {
-            func.apply(this, arguments);
-        }, delay);
-    };
+async function concurrentPromises(promises, maxConcurrent) {
+    const results = [];
+    const executingPromises = [];
+    for (let i = 0; i < promises.length; i++) {
+        const currentPromise = promises[i];
+        const promise = new Promise(async (resolve) => {
+            const result = await currentPromise();
+            results.push(result);
+            resolve();
+        });
+        executingPromises.push(promise);
+        if (
+            executingPromises.length >= maxConcurrent ||
+            i === promises.length - 1
+        ) {
+            await Promise.all(executingPromises);
+            const batchResults = results.slice(
+                results.length - executingPromises.length
+            );
+            console.log(batchResults);
+            executingPromises.length = 0;
+        }
+    }
 }
-const expensiveOperation = () => console.log("Виконую складну операцію...");
-const debouncedExpensiveOperation = debounce(expensiveOperation, 1000);
-debouncedExpensiveOperation();
-debouncedExpensiveOperation();
-debouncedExpensiveOperation();
+const promises = [
+    () =>
+        new Promise((resolve) => setTimeout(() => resolve("Promise 1"), 1000)),
+    () => new Promise((resolve) => setTimeout(() => resolve("Promise 2"), 500)),
+    () => new Promise((resolve) => setTimeout(() => resolve("Promise 3"), 800)),
+];
+concurrentPromises(promises, 2);
 
 //3 програма
-function intervalRace(functions, t) {
-    const results = [];
-    let index = 0;
-
-    const runNextFunction = () => {
-        if (index < functions.length) {
-            functions[index]().then((result) => {
-                results.push(result);
-                console.log(result);
-                index++;
-                if (index < functions.length) {
-                    setTimeout(runNextFunction, t);
-                }
-            });
-        }
-    };
-
-    runNextFunction();
+async function sequenceAsync(asyncFunctions) {
+    let result = null;
+    for (const asyncFunc of asyncFunctions) {
+        result = await asyncFunc(result);
+    }
+    return result;
 }
+const asyncFunc1 = async (prevResult) => {
+    // Очікуєм 1 секунду, а потім повертаємо значення 10
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return prevResult ? prevResult + 10 : 10;
+};
+const asyncFunc2 = async (prevResult) => {
+    // Очікуєм 500 мілісекунд, а потім повертаємо значення 20
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    return prevResult ? prevResult + 20 : 20;
+};
+const asyncFunc3 = async (prevResult) => {
+    // Очікуєм 800 мілісекунд, а потім повертаємо значення 30
+    await new Promise((resolve) => setTimeout(resolve, 800));
+    return prevResult ? prevResult + 30 : 30;
+};
+// Масив функцій-промісів, які будуть виконуватися послідовно
+const asyncFunctions = [asyncFunc1, asyncFunc2, asyncFunc3];
 
-// Приклад використання
-const task1 = () =>
-    new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("Завдання 1 виконано");
-        }, 1000);
-    });
-
-const task2 = () =>
-    new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("Завдання 2 виконано");
-        }, 500);
-    });
-
-const task3 = () =>
-    new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("Завдання 3 виконано");
-        }, 1500);
-    });
-
-const task4 = () =>
-    new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("Завдання 4 виконано");
-        }, 500);
-    });
-intervalRace([task1, task2, task3, task4], 1000);
+sequenceAsync(asyncFunctions).then((finalResult) => {
+    console.log("Кінцевий результат:", finalResult);
+});
